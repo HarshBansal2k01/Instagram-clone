2356ecd701600249d9080648e9350dedf25b949e
HarshBansal2k01 <ihbbro14@gmail.com>
2022-05-31 19:53:00 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // importing from global css
-import '../style/globals.css'
+import '../styles/globals.css'
 // import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
